d9ccad9bd8248740867cdc124602df1008684f11
S D Sahil <sd.sahil@epikindifi.com>
2023-01-24 23:02:39 +0530

    WIP:add gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  public isActive: Boolean = false;
+  /**
+   * handle
+   */
+  public handle(event?) {
+    this.isActive = true;
+  }
 }
