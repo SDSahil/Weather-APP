f03547a659d73e3187ee6978d6f73563f60b644c
SDSahil <1905555@kiit.ac.in>
2023-06-19 23:57:46 +0530

    WIP: angular 16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,79 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { lastValueFrom as toPromise } from "rxjs";
-import { HTTPService } from './services/http.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public isActive: Boolean = false;
-  public cityName: string = '';
-  public currCity: string = '';
-  public currTemp: string = '';
-  public currDesc: string = '';
-  public temp: string = '';
-  public desc: string = '';
-  public weatherInfo: any = {};
-  public currWeatherInfo: any = {};
-
-  constructor(private httpService: HTTPService) { }
-
-  async ngOnInit() {
-    this.currCity = 'mumbai';
-    const weatherData = await this.getWeatherData(this.currCity);
-    this.currTemp = weatherData?.main?.temp;
-    this.currDesc = weatherData?.weather[0]?.description;
-
-    this.currWeatherInfo.temp = weatherData?.main?.temp;
-    this.currWeatherInfo.desc = weatherData?.weather[0]?.description;
-    this.currWeatherInfo.cityName = this.currCity;
-  }
-
-  /**
-   * getLocation
-   */
-  public getLocation() {
-    const options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    };
-
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(resolve, reject, options);
-    });
-  }
-
-  /**
-   * getWeatherData
-   */
-  public getWeatherData(city: string) {
-    const apiKey = "69c0d37d33e433e8ddbc3bb12ed00f41";
-    const unit = "metric";
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=" + unit;
-
-    return toPromise(this.httpService.get(url));
-  }
-  /**
-   * handle
-   */
-  public async handle(event?) {
-    if (this.cityName) {
-      const weatherData = await this.getWeatherData(this.cityName);
-      console.log(weatherData);
-      this.temp = weatherData?.main?.temp;
-      this.desc = weatherData?.weather[0]?.description;
-
-      this.weatherInfo.temp = weatherData?.main?.temp;
-      this.weatherInfo.desc = weatherData?.weather[0]?.description;
-      this.weatherInfo.cityName = this.cityName;
-      this.isActive = true;
-    } else {
-      this.isActive = false
-      alert('Enter city');
-    }
-  }
+  title = 'weather-app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from "@angular/common/http";
-import { WeatherDetails } from "./weather/weather.component";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WeatherDetails
+    AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
